2c567cad80afe90846762ead4eec184842585ef0
Steve Montambeault <stevemo2003@gmail.com>
2024-09-23 13:54:59 +0800

    change default shield settings

--- config/filament-shield.php
+++ config/filament-shield.php
@@ -18,7 +18,7 @@ return [
     ],
 
     'super_admin' => [
-        'enabled'         => false,
+        'enabled'         => true,
         'name'            => 'super_admin',
         'define_via_gate' => false,
         'intercept_gate'  => 'before', // after
@@ -78,9 +78,9 @@ return [
     ],
 
     'discovery' => [
-        'discover_all_resources' => false,
-        'discover_all_widgets'   => false,
-        'discover_all_pages'     => false,
+        'discover_all_resources' => true,
+        'discover_all_widgets'   => true,
+        'discover_all_pages'     => true,
     ],
 
     'register_role_policy' => [
